87004f613cfee0c2c810eed1b115cfa5edc1900c
Raj <rajarshi@swivl.tech>
2025-07-21 20:24:38 +0530

    Express setup done. Prisma client setup and access verified

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello world")
+import express from 'express';
+import dotenv from 'dotenv';
+import prisma from './lib/prisma';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.get('/', async (_req, res) => {
+  const contacts = await prisma.contact.findMany();
+  res.json({ message: 'Up', totalContacts: contacts.length });
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
